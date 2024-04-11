f6e67c2aa7e2f8d704585fd1bbe0024eb318b75a
DariusTheGreat <MyFirstNameAndLastNameOneWord@gmail.com>
2024-04-10 21:15:18 -0700

    added solution files

---
+++ Brightness.Library/BrightnessController.cs
@@ -0,0 +1,16 @@
+﻿namespace Brightness.Library;
+
+public class  BrightnessController : IBrightnessController
+{
+	private readonly Lazy<PhysicalMonitorBrightnessController> _lazyLoaderdController;
+
+    public BrightnessController() => _lazyLoaderdController = new(() => new PhysicalMonitorBrightnessController());
+
+	private PhysicalMonitorBrightnessController Controller => _lazyLoaderdController.Value;
+
+    public uint Brightness 
+	{
+		get => Controller.Get();
+		set => Controller.Set(value);
+	}
+}
